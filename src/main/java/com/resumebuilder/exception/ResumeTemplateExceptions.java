7def83ee3b9dec59b2d29907d9dc1abd8b147817
vinit1453 <dondapativinit@gmail.com>
2023-10-09 19:13:07 +0530

    created Templates(Crud) Api by Vinit

---
+++ src/main/java/com/resumebuilder/exception/ResumeTemplateExceptions.java
@@ -0,0 +1,9 @@
+package com.resumebuilder.exception;
+
+public class ResumeTemplateExceptions extends RuntimeException {
+	private static final long serialVersionUID = -7571491666096541484L;
+
+	public ResumeTemplateExceptions(String Message) {
+		super(Message);
+	}
+}
