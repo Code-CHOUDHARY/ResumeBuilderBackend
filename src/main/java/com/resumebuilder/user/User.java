67cda216c2104218709b8822e905cc0d7282f9a3
pratikshawakekar <pratikshawa20@gmail.com>
2023-10-15 18:09:51 +0530

    Complete bulk upload for employee

--- src/main/java/com/resumebuilder/user/User.java
+++ src/main/java/com/resumebuilder/user/User.java
@@ -24,6 +24,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -103,7 +104,7 @@ public class User {
 		this.password = password;
 	}
 	
-	@OneToOne(cascade = CascadeType.ALL)
+	@ManyToOne(cascade = CascadeType.ALL)
 	private UserRole appRole;
 
 }
