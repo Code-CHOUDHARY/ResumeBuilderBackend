5af0b315b39e7a0f419b538307b888f5bcba0cd1
pratikshawakekar <pratikshawa20@gmail.com>
2023-11-16 10:23:39 +0530

    Resolve add employee issue

--- src/main/java/com/resumebuilder/user/UserRoleRepository.java
+++ src/main/java/com/resumebuilder/user/UserRoleRepository.java
@@ -2,12 +2,23 @@ package com.resumebuilder.user;
 
 import com.resumebuilder.security.approle.ERole;
 import com.resumebuilder.security.approle.UserRole;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRoleRepository extends JpaRepository<UserRole, Integer> {
     @Query("select u from UserRole u where u.name = ?1")
     UserRole findByName(ERole name);
 
+//    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.appRole.name = :roleName")
+//	boolean findByRoleName(@Param("roleName") ERole roleName);
+
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.appRole.name = :roleName")
+	boolean existsByRoleName(@Param("roleName") ERole roleAdmin);
 
 }
